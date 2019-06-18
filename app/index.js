37acec2c1972cdc090e6e40126492ee164b7d08c
Ryan Jasinski <jasinski.17@gmail.com>
2019-06-17 21:18:27 -0400

    working solution

--- app/index.js
+++ app/index.js
@@ -1,35 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import store from './store' // imported for you already
+import React from "react";
+import ReactDOM from "react-dom";
+import store, { incrementCounter } from "./store"; // imported for you already
 
 class Counter extends React.Component {
-  constructor () {
-    super()
-    this.state = {
-      count: 0
-    }
-    this.increment = this.increment.bind(this)
+  constructor() {
+    super();
+    this.state = store.getState();
+    this.increment = this.increment.bind(this);
   }
 
-  increment () {
-    this.setState({
-      count: this.state.count + 1
-    })
+  componentDidMount() {
+    this.unsubscribe = store.subscribe(() => {
+      //set the unsubscribe for later use, adds listener to store
+      return this.setState(store.getState());
+    });
   }
 
-  render () {
+  componentWillUnmount() {
+    this.unsubscribe(); //triggers off when the component is no longer being rendered
+  }
+
+  increment() {
+    console.log(incrementCounter());
+    store.dispatch(incrementCounter());
+  }
+
+  render() {
     return (
-      <div id='container'>
-        <div id='counter'>
+      <div id="container">
+        <div id="counter">
           <h1>{this.state.count}</h1>
           <button onClick={this.increment}>Increment</button>
         </div>
       </div>
-    )
+    );
   }
 }
 
-ReactDOM.render(
-  <Counter />,
-  document.getElementById('app')
-)
+ReactDOM.render(<Counter />, document.getElementById("app"));
--- app/store.js
+++ app/store.js
@@ -1,4 +1,4 @@
-import {createStore} from 'redux'
+import { createStore } from "redux";
 
 // Still need:
 //  - action type
@@ -8,15 +8,21 @@ import {createStore} from 'redux'
 
 const initialState = {
   // what should be on our initial state?
-}
+  count: 0
+};
+
+const INCREMENT_COUNTER = "INCREMENT_COUNTER";
+export const incrementCounter = () => ({ type: INCREMENT_COUNTER });
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case INCREMENT_COUNTER:
+      return { ...state, count: state.count + 1 };
     default:
-      return state
+      return state;
   }
-}
+};
 
-const store = createStore(reducer)
+const store = createStore(reducer);
 
-export default store
+export default store;
